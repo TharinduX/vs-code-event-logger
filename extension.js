25e3dea178c10f580704e7f4bd1e1f8c385ce951
Tharindu Jayasanka <hello@tharindu.me>
2023-11-13 15:29:56 +0530

    fix: data save error

--- extension.js
+++ extension.js
@@ -2,113 +2,135 @@ const vscode = require('vscode');
 const path = require('path');
 const axios = require('axios');
 
-/**
- * @param {vscode.ExtensionContext} context
- */
+let sessionActive = false;
+let apiKey = '';
+let apiUrl = '';
+
 async function activate(context) {
-  const apiUrl = context.globalState.get('apiUrl');
-  const userEmail = context.globalState.get('userEmail');
-  const apiKey = context.globalState.get('apiKey');
+  const fileLocation = vscode.Uri.joinPath(context.extensionUri, 'data.json');
 
-  // Check if the information has already been collected
-  if (!apiUrl || !userEmail || !apiKey) {
-    await collectTelemetryData(context);
-  }
+  try {
+    // Check if data.json exists
+    const fileExists = await vscode.workspace.fs.stat(fileLocation).then(
+      () => true,
+      () => false
+    );
 
-  const startTime = new Date();
-  sendTelemetryEvent(apiUrl, apiKey, 'sessionStarted', {
-    startTime,
-    email: userEmail,
-  });
+    if (fileExists) {
+      // If the file exists, read its content
+      const dataBuffer = await vscode.workspace.fs.readFile(fileLocation);
+      const storedData = JSON.parse(dataBuffer.toString());
+      apiKey = storedData.apiKey;
+      apiUrl = storedData.apiUrl;
 
-  // Listen for changes in the active text editor
-  vscode.window.onDidChangeActiveTextEditor((editor) => {
-    if (editor) {
-      const activeFile = path.basename(editor.document.fileName);
-      const timeSwitched = new Date();
-      sendTelemetryEvent(apiUrl, apiKey, 'fileSwitched', {
-        activeFile,
-        timeSwitched,
-        email: userEmail,
+      // Checking the validity of the stored API key
+      const checkResponse = await checkValidityAPI(apiKey);
+      await confirmAndSendTelemetryEvent(fileLocation, checkResponse);
+    } else {
+      // If the file doesn't exist, ask the user for the API key
+      apiKey = await vscode.window.showInputBox({
+        prompt: 'Enter API Key',
+        placeHolder: 'Get it from vscode.tharindu.me',
       });
+
+      // Check the validity of the API key
+      const checkResponse = await checkValidityAPI(apiKey);
+      await confirmAndSendTelemetryEvent(fileLocation, checkResponse);
     }
-  });
+  } catch (error) {
+    // Handle errors gracefully
+    console.error(error);
+    vscode.window.showErrorMessage(
+      'Error reading data.json. Please check the extension logs.'
+    );
+  }
 }
 
-async function collectTelemetryData(context) {
-  const apiUrl = await vscode.window.showInputBox({
-    prompt: 'Enter API URL for telemetry events',
-    placeHolder: 'https://example.com/api',
-  });
+async function confirmAndSendTelemetryEvent(fileLocation, checkResponse) {
+  if (!checkResponse.data.profile.valid) {
+    vscode.window.showErrorMessage(
+      'Stored API Key is invalid. Please re-enter.'
+    );
+    // Give the user a chance to correct the invalid API key
+    apiKey = await vscode.window.showInputBox({
+      prompt: 'Enter API Key',
+      placeHolder: 'Get it from vscode.tharindu.me',
+    });
+    //Check validity of the new API key
+    checkResponse = await checkValidityAPI(apiKey);
+    await confirmAndSendTelemetryEvent(fileLocation, checkResponse);
+  } else {
+    //Get latest data and save it to data.json
+    newUrl = checkResponse.data.profile.data.url;
+    newKey = checkResponse.data.profile.data.api;
+    dataToStore = { apiKey: newKey, apiUrl: newUrl };
+    const dataBuffer = Buffer.from(JSON.stringify(dataToStore), 'utf8');
+    await vscode.workspace.fs.writeFile(fileLocation, dataBuffer);
 
+    //Send start session event
+    vscode.window.showInformationMessage('Sending events to: ' + newUrl);
+    const startTime = new Date();
+    sendTelemetryEvent(newUrl, 'sessionStarted', { startTime });
+    sessionActive = true;
+
+    //Listen for changes in the active text editor
+    vscode.window.onDidChangeActiveTextEditor((editor) => {
+      if (editor) {
+        const activeFile = path.basename(editor.document.fileName);
+        const timeSwitched = new Date();
+        sendTelemetryEvent(newUrl, 'fileSwitched', {
+          activeFile,
+          timeSwitched,
+        });
+      }
+    });
+  }
+  return;
+}
+
+async function checkValidityAPI(apiKey) {
+  const checkUrl = `http://localhost:3000/api/check/${apiKey}`;
+
+  try {
+    const checkResponse = await axios.get(checkUrl);
+    return checkResponse;
+  } catch (error) {
+    if (error.response && error.response.status === 401) {
+      vscode.window.showErrorMessage('Invalid API Key. Please re-enter.');
+      newKey = await vscode.window.showInputBox({
+        prompt: 'Enter API Key',
+        placeHolder: 'Get it from vscode.tharindu.me',
+      });
+      return checkValidityAPI(newKey);
+    } else {
+      // Handle other errors
+      console.error(error);
+      vscode.window.showErrorMessage('Error checking API validity.');
+      throw error; // rethrow the error if it's not a 401
+    }
+  }
+}
+
+async function sendTelemetryEvent(apiUrl, eventName, data) {
   if (!apiUrl) {
-    vscode.window.showErrorMessage('API URL is required for telemetry events.');
-    return;
-  }
-
-  const email = await vscode.window.showInputBox({
-    prompt: 'Enter your email',
-    placeHolder: 'user@example.com',
-  });
-
-  if (!email) {
-    vscode.window.showErrorMessage('Email is required for telemetry events.');
-    return;
-  }
-
-  const apiKey = await vscode.window.showInputBox({
-    prompt: 'Enter API key for authentication',
-    placeHolder: 'YourAPIKeyHere',
-  });
-
-  if (!apiKey) {
-    vscode.window.showErrorMessage('API key is required for telemetry events.');
-    return;
-  }
-
-  // Save the collected data to the extension's global state
-  context.globalState.update('apiUrl', apiUrl);
-  context.globalState.update('userEmail', email);
-  context.globalState.update('apiKey', apiKey);
-}
-
-// This method is called when your extension is deactivated
-function deactivate() {
-  // Deactivation logic if needed
-}
-
-async function sendTelemetryEvent(apiUrl, apiKey, eventName, data) {
-  // Retrieve the API URL and email from the extension's global state
-  const url =
-    apiUrl || vscode.workspace.getConfiguration().get('yourExtension.apiUrl');
-  const email =
-    data.email ||
-    vscode.workspace.getConfiguration().get('yourExtension.userEmail');
-
-  if (!url) {
     vscode.window.showErrorMessage(
       'API URL is not set. Please set it in extension settings.'
     );
     return;
   }
-
   const body = JSON.stringify({ eventName, data });
-
   try {
-    // Use 'axios' library to make a POST request to the API URL with API key
-    const response = await axios.post(url, body, {
-      headers: {
-        'Content-Type': 'application/json',
-        'X-API-Key': apiKey,
-      },
+    const response = await axios.post(apiUrl, body, {
+      headers: { 'Content-Type': 'application/json' },
     });
-    console.log(response.data);
   } catch (error) {
     console.error(error);
+    vscode.window.showErrorMessage(
+      'Check your receiving URL in extension settings, and restart the VS Code.'
+    );
   }
 }
 
 module.exports = {
   activate,
-  deactivate,
 };
